7916504a7872064b7d103cde03d57da02a141db8
Ganesha O'Donnell <go-donne@3-C-5.42heilbronn.de>
2025-09-04 14:48:55 +0200

    restructure render_complete_frame(): extract col rendering for norm compliance, for>while loop, removed UI phase prev idea (not in mandatory requirements)

--- src/render/screen_buffer.c
+++ src/render/screen_buffer.c
@@ -1,26 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   screen_buffer.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: go-donne <go-donne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 14:04:27 by go-donne          #+#    #+#             */
+/*   Updated: 2025/09/04 14:35:17 by go-donne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Rasterization and pixel buffer operations
+direct screen buffer ops */
+
 #include "../inc/cub3d.h"
 
 /* screen buffer management:
-clear & present frame buffer ops */
-void clear_screen_buffer(void)
+clear & present frame buffer ops
+
+. calc total pixels in screen buffer
+. get ptr to image data as int array for faster clearing
+. clear entire screen to black (0x000000) */
+void	clear_screen_buffer(void)
 {
-    int total_pixels;
-    int *pixel_data;
-    int i;
-    
-    // Calculate total pixels in screen buffer
-    total_pixels = g_game.graphics.screen_width * g_game.graphics.screen_height;
-    
-    // Get pointer to image data as integer array for faster clearing
-    pixel_data = (int *)g_game.graphics.image_data;
-    
-    // Clear entire screen to black (0x000000)
-    i = 0;
-    while (i < total_pixels)
-    {
-        pixel_data[i] = 0x000000;
-        i++;
-    }
+	int	total_pixels;
+	int	*pixel_data;
+	int	i;
+
+	total_pixels = g_game.graphics.screen_width
+					* g_game.graphics.screen_height;
+	pixel_data = (int *)g_game.graphics.image_data;
+	i = 0;
+	while (i < total_pixels)
+	{
+		pixel_data[i] = 0x000000;
+		i++;
+	}
 }
 
 void present_frame_to_screen(void)
@@ -39,18 +54,18 @@ void put_pixel(int x, int y, int color)
 {
     int pixel_index;
     int *pixel_data;
-    
+
     // Bounds checking
-    if (x < 0 || x >= g_game.graphics.screen_width || 
+    if (x < 0 || x >= g_game.graphics.screen_width ||
         y < 0 || y >= g_game.graphics.screen_height)
         return;
-    
+
     // Calculate pixel position in buffer
     pixel_index = y * g_game.graphics.screen_width + x;
-    
+
     // Get pixel data as integer array
     pixel_data = (int *)g_game.graphics.image_data;
-    
+
     // Set pixel color
     pixel_data[pixel_index] = color;
 }

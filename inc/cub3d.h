0e00b439ce690fb0f279522cd4c38e94c714e55a
blohrer7 <benjamin-lohrer@web.de>
2025-09-25 10:02:51 +0200

    norminette ready headers

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: go-donne <go-donne@student.42.fr>          +#+  +:+       +#+        */
+/*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 09:16:57 by go-donne          #+#    #+#             */
-/*   Updated: 2025/09/25 09:18:00 by go-donne         ###   ########.fr       */
+/*   Updated: 2025/09/25 10:02:13 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 /* ⭐⭐ INCLUDES */
 /* system */
 # include <fcntl.h>
+# include <stdint.h> /* <-- for uint32_t */
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdint.h>   /* <-- for uint32_t */
 
 /* math */
 # include <math.h>
@@ -71,7 +71,7 @@ typedef enum e_cell
 	CELL_VOID = -1,
 	CELL_EMPTY = 0,
 	CELL_WALL = 1
-}	t_cell;
+}					t_cell;
 
 /* ⭐ PARSER STATE FLAGS */
 enum
@@ -92,7 +92,7 @@ typedef struct s_texture_image
 	uint32_t		*pixels;
 	int				image_width;
 	int				image_height;
-}	t_texture_image;
+}					t_texture_image;
 
 /* ⭐ WORLD ENTITY REPRESENTATION */
 /* player state */
@@ -104,7 +104,7 @@ typedef struct s_player
 	double			world_dir_y;
 	double			world_camera_plane_x;
 	double			world_camera_plane_y;
-}	t_player;
+}					t_player;
 
 /* map configuration */
 typedef struct s_map
@@ -117,7 +117,7 @@ typedef struct s_map
 	uint32_t		ceiling_color;
 	char			*first_map_line;
 	t_texture_image	wall_textures[4];
-}	t_map;
+}					t_map;
 
 /* ⭐ SYSTEM INTERFACES */
 /* graphics system */
@@ -127,7 +127,7 @@ typedef struct s_graphics
 	mlx_image_t		*frame;
 	int				screen_width;
 	int				screen_height;
-}	t_graphics;
+}					t_graphics;
 
 /* ⭐ UTILITIES */
 typedef struct s_ffctx
@@ -139,7 +139,7 @@ typedef struct s_ffctx
 	int				h;
 	int				head;
 	int				tail;
-}	t_ffctx;
+}					t_ffctx;
 
 /* parser contexts */
 typedef struct s_texinfo
@@ -149,7 +149,7 @@ typedef struct s_texinfo
 	int				j;
 	int				bit;
 	int				*flags;
-}	t_texinfo;
+}					t_texinfo;
 
 /* ⭐ ORCHESTRATION */
 /* game state */
@@ -163,103 +163,110 @@ typedef struct s_game
 	double			rotation_speed;
 	double			time_prev;
 	double			time_now;
-}	t_game;
+}					t_game;
 
 /* ⭐⭐ FUNCTION PROTOTYPES */
 /* ================== PARSE_UTILS ================== */
-int		skip_ws(const char *s, int i);
-int		parse_u8_component(const char *s, int *i, int *out);
-int		expect_comma(const char *s, int *i);
-int		parse_rgb_triplet(const char *s, uint32_t *out_rgb);
-int		open_rdonly(const char *path);
+int					skip_ws(const char *s, int i);
+int					parse_u8_component(const char *s, int *i, int *out);
+int					expect_comma(const char *s, int *i);
+int					parse_rgb_triplet(const char *s, uint32_t *out_rgb);
+int					open_rdonly(const char *path);
 
 /* ================== PARSE_HEADER ================== */
-int		set_texture_field(char **dst_path, const char *after_key);
-int		set_color_field(uint32_t *dst_rgb, const char *after_key, int *was_set);
-int		parse_header_line(t_map *m, const char *line, int *flags);
-int		match2(const char *s, int i, char a, char b);
-int		set_tex_entry(char **slot, const char *after_key, int bit, int *flags);
-int		parse_header_texture(t_map *m, const char *line, int i, int *flags);
-int		parse_header_color(t_map *m, const char *line, int i, int *flags);
+int					set_texture_field(char **dst_path, const char *after_key);
+int					set_color_field(uint32_t *dst_rgb, const char *after_key,
+						int *was_set);
+int					parse_header_line(t_map *m, const char *line, int *flags);
+int					match2(const char *s, int i, char a, char b);
+int					set_tex_entry(char **slot, const char *after_key, int bit,
+						int *flags);
+int					parse_header_texture(t_map *m, const char *line, int i,
+						int *flags);
+int					parse_header_color(t_map *m, const char *line, int i,
+						int *flags);
 
 /* ================== PARSE_HEADER LINES ================== */
-int		parse_header_lines(t_map *m, int fd);
-int		check_texture_paths_exist(const t_map *m);
+int					parse_header_lines(t_map *m, int fd);
+int					check_texture_paths_exist(const t_map *m);
 
 /* ================== COLLECT_MAP_LINES ================== */
-int		collect_map_lines(t_map *m, int fd, char ***out_lines, int *out_h);
-int		append_line(char ***lines_ptr, int *cap_ptr, int *h_ptr, char *take);
-int		validate_map_line(const char *s);
-void	rstrip_eol(char *s);
+int					collect_map_lines(t_map *m, int fd, char ***out_lines,
+						int *out_h);
+int					append_line(char ***lines_ptr, int *cap_ptr, int *h_ptr,
+						char *take);
+int					validate_map_line(const char *s);
+void				rstrip_eol(char *s);
 
 /* ================== NORMALIZE_MAP ================== */
-int		normalize_map(char ***lines_io, int h, int *out_w);
+int					normalize_map(char ***lines_io, int h, int *out_w);
 
 /* ================== MAP_GRID_CELL ================== */
-int		put_cell_from_char(t_map *m, t_player *pl, int idx, char c);
+int					put_cell_from_char(t_map *m, t_player *pl, int idx, char c);
 
 /* ================== MAP_GRID_FILL ================== */
-int		fill_grid(t_map *m, t_player *pl, char **lines, int *count);
+int					fill_grid(t_map *m, t_player *pl, char **lines, int *count);
 
 /* ================== MAP_GRID_BUILD ================== */
-int		build_grid_from_lines(t_map *m, t_player *pl, char **lines,
-			int *player_found);
+int					build_grid_from_lines(t_map *m, t_player *pl, char **lines,
+						int *player_found);
 
 /* ================== MAP_CHECK_FLOOD ================== */
-void	ffctx_init(t_ffctx *c, const t_map *m, char *vis, int *q);
-int		flood_from_start(t_ffctx *c, int start_idx);
-int		map_has_open_border(const t_map *m);
-int		border_rows_have_empty(const t_map *m, int w, int h);
-int		border_cols_have_empty(const t_map *m, int w, int h);
-int		cell_adjacent_to_void(const t_map *m, int x, int y);
-int		has_empty_adjacent_to_void(const t_map *m);
+void				ffctx_init(t_ffctx *c, const t_map *m, char *vis, int *q);
+int					flood_from_start(t_ffctx *c, int start_idx);
+int					map_has_open_border(const t_map *m);
+int					border_rows_have_empty(const t_map *m, int w, int h);
+int					border_cols_have_empty(const t_map *m, int w, int h);
+int					cell_adjacent_to_void(const t_map *m, int x, int y);
+int					has_empty_adjacent_to_void(const t_map *m);
 
 /* ================== MAP_CHECK_UTILS ================== */
-int		idx_2d_to_1d(int x, int y, int w);
-int		in_bounds(int x, int y, int w, int h);
-void	queue_push(int *q, int *tail, int v);
-int		queue_pop(int *q, int *head, int tail, int *out);
-int		load_textures_or_fail(t_game *g);
+int					idx_2d_to_1d(int x, int y, int w);
+int					in_bounds(int x, int y, int w, int h);
+void				queue_push(int *q, int *tail, int v);
+int					queue_pop(int *q, int *head, int tail, int *out);
+int					load_textures_or_fail(t_game *g);
 
 /* ================== MAP_CHECK_FLOOD ================== */
-int		flood_from_border(t_ffctx *c);
-int		map_is_closed(const t_map *m);
+int					flood_from_border(t_ffctx *c);
+int					map_is_closed(const t_map *m);
 
 /* ================== GAME_LOOP ================== */
-double	get_delta_time(t_game *g);
-void	rotate_player(t_game *g, double angle);
-void	try_move_player(t_game *g, double new_x, double new_y);
-void	handle_input(t_game *g, double dt);
-void	game_loop_tick(void *param);
-void	on_close(void *param);
-void	handle_exit_input(t_game *g);
-void	handle_rotation_input(t_game *g, double ang);
-int		map_cell(const t_map *m, int ix, int iy);
+double				get_delta_time(t_game *g);
+void				rotate_player(t_game *g, double angle);
+void				try_move_player(t_game *g, double new_x, double new_y);
+void				handle_input(t_game *g, double dt);
+void				game_loop_tick(void *param);
+void				on_close(void *param);
+void				handle_exit_input(t_game *g);
+void				handle_rotation_input(t_game *g, double ang);
+int					map_cell(const t_map *m, int ix, int iy);
 
 /* ================== UTILS ================== */
-void	parse_error(const char *msg);
-int		check_cub_extension(const char *filename);
+void				parse_error(const char *msg);
+int					check_cub_extension(const char *filename);
 
 /* ================== GRAPHICS / WINDOW ================== */
-int		gfx_open_window_resizable(t_game *game,
-			const char *title, int w, int h);
-int		gfx_maximize_now(t_graphics *g);
-int		gfx_rebuild_framebuffer(t_graphics *g, int w, int h);
-void	handle_resize(int32_t w, int32_t h, void *param);
+int					gfx_open_window_resizable(t_game *game, const char *title,
+						int w, int h);
+int					gfx_maximize_now(t_graphics *g);
+int					gfx_rebuild_framebuffer(t_graphics *g, int w, int h);
+void				handle_resize(int32_t w, int32_t h, void *param);
 
 /* ================== LOAD_TEXTURE ================== */
-int		load_one_texture_png(const char *path, t_texture_image *out);
-int		load_all_wall_textures(t_map *m);
+int					load_one_texture_png(const char *path,
+						t_texture_image *out);
+int					load_all_wall_textures(t_map *m);
 
 /* ================== CLEANUP ================== */
-void	free_lines_array(char **lines, int h);
-void	free_map(t_game *g, t_map *m);
-int		cleanup_and_exit(t_game *g, int code);
+void				free_lines_array(char **lines, int h);
+void				free_map(t_game *g, t_map *m);
+int					cleanup_and_exit(t_game *g, int code);
 
 /* ================== RUNTIME_INIT ================== */
-void	init_runtime_defaults(t_game *g);
-int		start_window(t_game *g, const char *title, int w, int h);
-void	sync_player_world_fields_from_parser(t_game *g);
-int		map_load_and_validate(t_game *g, const char *path);
+void				init_runtime_defaults(t_game *g);
+int					start_window(t_game *g, const char *title, int w, int h);
+void				sync_player_world_fields_from_parser(t_game *g);
+int					map_load_and_validate(t_game *g, const char *path);
 
 #endif
--- inc/render.h
+++ inc/render.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: go-donne <go-donne@student.42.fr>          +#+  +:+       +#+        */
+/*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 09:18:15 by go-donne          #+#    #+#             */
-/*   Updated: 2025/09/25 09:27:29 by go-donne         ###   ########.fr       */
+/*   Updated: 2025/09/25 10:02:34 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,99 +15,101 @@
 
 # include "cub3d.h"
 
-# define VERTICAL_WALL    0
-# define HORIZONTAL_WALL  1
+# define VERTICAL_WALL 0
+# define HORIZONTAL_WALL 1
 
 // screen-space positioning data for sgl col
 typedef struct s_screen_column_bounds
 {
-    int column_x;
-    int wall_start_y;  
-    int wall_end_y;
-} t_screen_column_bounds;
+	int			column_x;
+	int			wall_start_y;
+	int			wall_end_y;
+}				t_screen_column_bounds;
 
 typedef struct s_dda_state
 {
-	int		map_x;
-	int		map_y;
-	int		step_x;
-	int		step_y;
-	double	delta_dist_x;
-	double	delta_dist_y;
-	double	world_dist_to_next_boundary_x;
-	double	world_dist_to_next_boundary_y;
-	double	world_ray_dir_x;
-	double	world_ray_dir_y;
-	int		wall_intersection_found;
-}	t_dda_state;
+	int			map_x;
+	int			map_y;
+	int			step_x;
+	int			step_y;
+	double		delta_dist_x;
+	double		delta_dist_y;
+	double		world_dist_to_next_boundary_x;
+	double		world_dist_to_next_boundary_y;
+	double		world_ray_dir_x;
+	double		world_ray_dir_y;
+	int			wall_intersection_found;
+}				t_dda_state;
 
 typedef struct s_ray_result
 {
-	double	world_intersection_x;
-	double	world_intersection_y;
-	int		world_wall_side;               // vert / horiz
-	double	world_perpendicular_distance;
-	int		world_wall_face;
-}	t_ray_result;
+	double		world_intersection_x;
+	double		world_intersection_y;
+	int			world_wall_side;
+	double		world_perpendicular_distance;
+	int			world_wall_face;
+}				t_ray_result;
 
 typedef struct s_texture_context
 {
-	int		world_wall_face;
-	double	world_wall_intersection_x;
-	double	world_wall_intersection_y;
-	int		screen_wall_height;
-}	t_texture_context;
+	int			world_wall_face;
+	double		world_wall_intersection_x;
+	double		world_wall_intersection_y;
+	int			screen_wall_height;
+}				t_texture_context;
 
-void		render_complete_frame(t_game *g);
-void		render_single_column(t_game *g, int screen_column_x);
-void render_wall_column(t_game *g, int screen_column_x,
-	t_ray_result *wall_intersection_data, int projected_wall_height);
-void		render_ceiling_section(t_game *g, int screen_column_x,
-				int wall_start_y_pixel);
-void render_wall_section(t_game *g, t_screen_column_bounds *screen_bounds,
+void			render_complete_frame(t_game *g);
+void			render_single_column(t_game *g, int screen_column_x);
+void			render_wall_column(t_game *g, int screen_column_x,
+					t_ray_result *wall_intersection_data,
+					int projected_wall_height);
+void			render_ceiling_section(t_game *g, int screen_column_x,
+					int wall_start_y_pixel);
+void			render_wall_section(t_game *g,
+					t_screen_column_bounds *screen_bounds,
 					t_ray_result *wall_hit_data);
-void		render_floor_section(t_game *g, int screen_column_x,
-				int wall_end_y_pixel);
+void			render_floor_section(t_game *g, int screen_column_x,
+					int wall_end_y_pixel);
 
-void		calculate_ray_direction(t_game *g, int screen_column_x,
-				double *world_ray_direction_x,
-				double *world_ray_direction_y);
+void			calculate_ray_direction(t_game *g, int screen_column_x,
+					double *world_ray_direction_x,
+					double *world_ray_direction_y);
 t_ray_result	cast_ray_to_wall(t_game *g, double world_ray_dir_x,
-				double world_ray_dir_y);
-int			determine_intersected_wall_face(t_game *g,
-				t_ray_result *wall_intersection_data);
+					double world_ray_dir_y);
+int				determine_intersected_wall_face(t_game *g,
+					t_ray_result *wall_intersection_data);
 
-void		setup_dda_vars(t_game *g, double ray_dir_x, double ray_dir_y,
-				t_dda_state *state);
-void		execute_dda_traversal(t_game *g, t_dda_state *dda_state,
-				int *world_wall_side);
-double		calculate_wall_distance(t_game *g, t_dda_state *dda_state,
-				int world_wall_side);
+void			setup_dda_vars(t_game *g, double ray_dir_x, double ray_dir_y,
+					t_dda_state *state);
+void			execute_dda_traversal(t_game *g, t_dda_state *dda_state,
+					int *world_wall_side);
+double			calculate_wall_distance(t_game *g, t_dda_state *dda_state,
+					int world_wall_side);
 
-int			calculate_screen_wall_height(t_game *g,
-				double world_wall_distance);
-void		simulate_eye_level_perspective(t_game *g, int wall_height,
-				int *wall_start, int *wall_end);
-void		centre_wall_at_eye_level(t_game *g, int wall_height,
-				int *wall_start, int *wall_end);
-void		enforce_screen_pixel_boundaries(t_game *g, int *wall_start,
-				int *wall_end);
+int				calculate_screen_wall_height(t_game *g,
+					double world_wall_distance);
+void			simulate_eye_level_perspective(t_game *g, int wall_height,
+					int *wall_start, int *wall_end);
+void			centre_wall_at_eye_level(t_game *g, int wall_height,
+					int *wall_start, int *wall_end);
+void			enforce_screen_pixel_boundaries(t_game *g, int *wall_start,
+					int *wall_end);
 
-double		world_wall_texture_u(t_texture_context *ctx);
-double		screen_wall_texture_v(t_game *g, t_texture_context *ctx,
-				int curr_px_y);
-int			screen_pixel_texture_colour(t_game *g, t_texture_context *ctx,
-				int current_pixel_y);
-int			texture_pixel_colour(t_texture_image *tx_img, int tx_px_x,
-				int tx_px_y);
-double		safe_fractional_part(double coordinate);
-int			clamp_texture_pixel(int pixel_coord, int max_dimension);
+double			world_wall_texture_u(t_texture_context *ctx);
+double			screen_wall_texture_v(t_game *g, t_texture_context *ctx,
+					int curr_px_y);
+int				screen_pixel_texture_colour(t_game *g, t_texture_context *ctx,
+					int current_pixel_y);
+int				texture_pixel_colour(t_texture_image *tx_img, int tx_px_x,
+					int tx_px_y);
+double			safe_fractional_part(double coordinate);
+int				clamp_texture_pixel(int pixel_coord, int max_dimension);
 
-void		clear_screen_buffer(t_game *g);
-void		put_pixel(t_game *g, int screen_x, int screen_y, int pixel_color);
+void			clear_screen_buffer(t_game *g);
+void			put_pixel(t_game *g, int screen_x, int screen_y,
+					int pixel_color);
 
-
-void		setup_x_step(t_game *g, double ray_dir_x, t_dda_state *state);
-void		setup_y_step(t_game *g, double ray_dir_y, t_dda_state *state);
+void			setup_x_step(t_game *g, double ray_dir_x, t_dda_state *state);
+void			setup_y_step(t_game *g, double ray_dir_y, t_dda_state *state);
 
 #endif

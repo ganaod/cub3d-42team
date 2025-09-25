a35d10de3f574205ed3d4473e196765ed1cac2e1
Ganesha O'Donnell <go-donne@2-D-2.42heilbronn.de>
2025-09-25 11:30:16 +0200

    norm conform prog

--- inc/render.h
+++ inc/render.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: go-donne <go-donne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/25 09:18:15 by go-donne          #+#    #+#             */
-/*   Updated: 2025/09/25 11:25:03 by go-donne         ###   ########.fr       */
+/*   Created: 2025/09/25 11:30:02 by go-donne          #+#    #+#             */
+/*   Updated: 2025/09/25 11:30:04 by go-donne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,47 @@
 
 # include "cub3d.h"
 
-# define VERTICAL_WALL 0
-# define HORIZONTAL_WALL 1
+# define VERTICAL_WALL		0
+# define HORIZONTAL_WALL	1
 
 // screen-space positioning data for sgl col
 typedef struct s_screen_column_bounds
 {
-	int			column_x;
-	int			wall_start_y;
-	int			wall_end_y;
+	int				column_x;
+	int				wall_start_y;
+	int				wall_end_y;
 }				t_screen_column_bounds;
 
 typedef struct s_dda_state
 {
-	int			map_x;
-	int			map_y;
-	int			step_x;
-	int			step_y;
-	double		delta_dist_x;
-	double		delta_dist_y;
-	double		world_dist_to_next_boundary_x;
-	double		world_dist_to_next_boundary_y;
-	double		world_ray_dir_x;
-	double		world_ray_dir_y;
-	int			wall_intersection_found;
+	int				map_x;
+	int				map_y;
+	int				step_x;
+	int				step_y;
+	double			delta_dist_x;
+	double			delta_dist_y;
+	double			world_dist_to_next_boundary_x;
+	double			world_dist_to_next_boundary_y;
+	double			world_ray_dir_x;
+	double			world_ray_dir_y;
+	int				wall_intersection_found;
 }				t_dda_state;
 
 typedef struct s_ray_result
 {
-	double		world_intersection_x;
-	double		world_intersection_y;
-	int			world_wall_side;
-	double		world_perpendicular_distance;
-	int			world_wall_face;
+	double			world_intersection_x;
+	double			world_intersection_y;
+	int				world_wall_side;
+	double			world_perpendicular_distance;
+	int				world_wall_face;
 }				t_ray_result;
 
 typedef struct s_texture_context
 {
-	int			world_wall_face;
-	double		world_wall_intersection_x;
-	double		world_wall_intersection_y;
-	int			screen_wall_height;
+	int				world_wall_face;
+	double			world_wall_intersection_x;
+	double			world_wall_intersection_y;
+	int				screen_wall_height;
 }				t_texture_context;
 
 typedef struct s_texture_step_data
@@ -68,17 +68,14 @@ typedef struct s_texture_step_data
 	int				end_y;
 }				t_texture_step_data;
 
-
-/* ⭐⭐ FUNCTION PROTOTYPES */
-
-/* ================== FRAME ORCHESTRATION ================== */
+// frame orchestration
 void			render_complete_frame(t_game *g);
 void			render_single_column(t_game *g, int screen_column_x);
 void			render_wall_column(t_game *g, int screen_column_x,
 					t_ray_result *wall_intersection_data,
 					int projected_wall_height);
 
-/* ================== COLUMN SECTION RENDERING ================== */
+// column section rendering
 void			render_ceiling_section(t_game *g, int screen_column_x,
 					int wall_start_y_pixel);
 void			render_wall_section(t_game *g,
@@ -87,7 +84,7 @@ void			render_wall_section(t_game *g,
 void			render_floor_section(t_game *g, int screen_column_x,
 					int wall_end_y_pixel);
 
-/* ================== RAY CASTING ================== */
+// ray casting
 void			calculate_ray_direction(t_game *g, int screen_column_x,
 					double *world_ray_direction_x,
 					double *world_ray_direction_y);
@@ -96,7 +93,7 @@ t_ray_result	cast_ray_to_wall(t_game *g, double world_ray_dir_x,
 int				determine_intersected_wall_face(t_game *g,
 					t_ray_result *wall_intersection_data);
 
-/* ================== DDA ALGORITHM ================== */
+// dda algorithm
 void			setup_dda_vars(t_game *g, double ray_dir_x, double ray_dir_y,
 					t_dda_state *state);
 void			execute_dda_traversal(t_game *g, t_dda_state *dda_state,
@@ -106,7 +103,7 @@ double			calculate_wall_distance(t_game *g, t_dda_state *dda_state,
 void			setup_x_step(t_game *g, double ray_dir_x, t_dda_state *state);
 void			setup_y_step(t_game *g, double ray_dir_y, t_dda_state *state);
 
-/* ================== PROJECTION ================== */
+// projection
 int				calculate_screen_wall_height(t_game *g,
 					double world_wall_distance);
 void			simulate_eye_level_perspective(t_game *g, int wall_height,
@@ -116,7 +113,7 @@ void			centre_wall_at_eye_level(t_game *g, int wall_height,
 void			enforce_screen_pixel_boundaries(t_game *g, int *wall_start,
 					int *wall_end);
 
-/* ================== TEXTURE MAPPING ================== */
+// texture mapping
 double			world_wall_texture_u(t_texture_context *ctx);
 double			safe_fractional_part(double coordinate);
 int				setup_wall_texture_data(t_game *g,
@@ -125,10 +122,10 @@ int				setup_wall_texture_data(t_game *g,
 void			calculate_texture_stepping(t_texture_step_data *data);
 void			render_textured_pixels(t_game *g, int column_x,
 					t_texture_step_data *data);
-uint32_t	    sample_texture_pixel(t_texture_image *texture,
-                    int tex_x, int tex_y);
+uint32_t		sample_texture_pixel(t_texture_image *texture,
+					int tex_x, int tex_y);
 
-/* ================== PIXEL BUFFER ================== */
+// pixel buffer
 void			clear_screen_buffer(t_game *g);
 void			put_pixel(t_game *g, int screen_x, int screen_y,
 					int pixel_color);
